754c06015184395026fedc450cd91fe960a73e37
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-07-30 13:22:22 -0500

    Adding last proyects in work section

---
+++ main.js
@@ -0,0 +1,28 @@
+const btnDocs1 = document.getElementById('btn-1');
+const btnDocs2 = document.getElementById('btn-2');
+const btnDocs3 = document.getElementById('btn-3');
+const btnDocs4 = document.getElementById('btn-4');
+const btnDocs5 = document.getElementById('btn-5');
+const btnDocs6 = document.getElementById('btn-6');
+const btnDocs7 = document.getElementById('btn-7');
+
+btnDocs3.addEventListener('click', () => {
+    window.open("https://github.com/AliceRamirez17/LIM012-data-lovers", "_blank")
+});
+
+btnDocs4.addEventListener('click', () => {
+    window.open("https://aliceramirez17.github.io/LIM012-data-lovers/src/", "_blank")
+});
+
+btnDocs5.addEventListener('click', () => {
+    window.open("https://github.com/AliceRamirez17/LIM012-fe-social-network/tree/gh-pages", "_blank")
+});
+
+btnDocs6.addEventListener('click', () => {
+    window.open("https://aliceramirez17.github.io/LIM012-fe-social-network/src/#/signin", "_blank")
+});
+
+btnDocs7.addEventListener('click', () => {
+    window.open("https://github.com/AliceRamirez17/LIM012-fe-md-links", "_blank")
+});
+
