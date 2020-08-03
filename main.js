c627f5e7561b74d04e1b6c81ba15e79692b82009
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-08-03 01:26:47 -0500

    Adding contact section

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ const btnDocs5 = document.getElementById('btn-5');
 const btnDocs6 = document.getElementById('btn-6');
 const btnDocs7 = document.getElementById('btn-7');
 
+btnDocs1.addEventListener('click', () => {
+    window.open("https://github.com/AliceRamirez17/LIM012-card-validation", "_blank")
+});
+
+btnDocs2.addEventListener('click', () => {
+    window.open("https://aliceramirez17.github.io/LIM012-card-validation/src/", "_blank")
+});
+
 btnDocs3.addEventListener('click', () => {
     window.open("https://github.com/AliceRamirez17/LIM012-data-lovers", "_blank")
 });
@@ -24,5 +32,21 @@ btnDocs6.addEventListener('click', () => {
 
 btnDocs7.addEventListener('click', () => {
     window.open("https://github.com/AliceRamirez17/LIM012-fe-md-links", "_blank")
+});
+
+const btnLinkedin = document.getElementById('btnLinkedin');
+const btnGithub = document.getElementById('btnGithub');
+const btnBehance = document.getElementById('btnBehance')
+
+btnLinkedin.addEventListener('click', () => {
+    window.open("https://www.linkedin.com/in/alice-ram%C3%ADrez-developer/", "_blank")
+});
+
+btnGithub.addEventListener('click', () => {
+    window.open("https://github.com/AliceRamirez17", "_blank")
+});
+
+btnBehance.addEventListener('click', () => {
+    window.open("https://www.behance.net/thecatgirl", "_blank")
 });
 
