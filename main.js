cceab28af2055865194668960560b1f6ae01781d
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-11-19 22:59:23 -0500

    Adding BQ-api-cliente repository

--- main.js
+++ main.js
@@ -1,52 +1,77 @@
-const btnDocs1 = document.getElementById('btn-1');
-const btnDocs2 = document.getElementById('btn-2');
-const btnDocs3 = document.getElementById('btn-3');
-const btnDocs4 = document.getElementById('btn-4');
-const btnDocs5 = document.getElementById('btn-5');
-const btnDocs6 = document.getElementById('btn-6');
-const btnDocs7 = document.getElementById('btn-7');
+const btnDocs1 = document.getElementById("btn-1");
+const btnDocs2 = document.getElementById("btn-2");
+const btnDocs3 = document.getElementById("btn-3");
+const btnDocs4 = document.getElementById("btn-4");
+const btnDocs5 = document.getElementById("btn-5");
+const btnDocs6 = document.getElementById("btn-6");
+const btnDocs7 = document.getElementById("btn-7");
+const btnDocs9 = document.getElementById("btn-9");
 
-btnDocs1.addEventListener('click', () => {
-    window.open("https://github.com/AliceRamirez17/LIM012-card-validation", "_blank")
+btnDocs1.addEventListener("click", () => {
+  window.open(
+    "https://github.com/AliceRamirez17/LIM012-card-validation",
+    "_blank"
+  );
 });
 
-btnDocs2.addEventListener('click', () => {
-    window.open("https://aliceramirez17.github.io/LIM012-card-validation/src/", "_blank")
+btnDocs2.addEventListener("click", () => {
+  window.open(
+    "https://aliceramirez17.github.io/LIM012-card-validation/src/",
+    "_blank"
+  );
 });
 
-btnDocs3.addEventListener('click', () => {
-    window.open("https://github.com/AliceRamirez17/LIM012-data-lovers", "_blank")
+btnDocs3.addEventListener("click", () => {
+  window.open("https://github.com/AliceRamirez17/LIM012-data-lovers", "_blank");
 });
 
-btnDocs4.addEventListener('click', () => {
-    window.open("https://aliceramirez17.github.io/LIM012-data-lovers/src/", "_blank")
+btnDocs4.addEventListener("click", () => {
+  window.open(
+    "https://aliceramirez17.github.io/LIM012-data-lovers/src/",
+    "_blank"
+  );
 });
 
-btnDocs5.addEventListener('click', () => {
-    window.open("https://github.com/AliceRamirez17/LIM012-fe-social-network/tree/gh-pages", "_blank")
+btnDocs5.addEventListener("click", () => {
+  window.open(
+    "https://github.com/AliceRamirez17/LIM012-fe-social-network/tree/gh-pages",
+    "_blank"
+  );
 });
 
-btnDocs6.addEventListener('click', () => {
-    window.open("https://aliceramirez17.github.io/LIM012-fe-social-network/src/#/signin", "_blank")
+btnDocs6.addEventListener("click", () => {
+  window.open(
+    "https://aliceramirez17.github.io/LIM012-fe-social-network/src/#/signin",
+    "_blank"
+  );
 });
 
-btnDocs7.addEventListener('click', () => {
-    window.open("https://aliceramirez17.github.io/LIM012-fe-md-links/", "_blank")
+btnDocs7.addEventListener("click", () => {
+  window.open("https://aliceramirez17.github.io/LIM012-fe-md-links/", "_blank");
 });
 
-const btnLinkedin = document.getElementById('btnLinkedin');
-const btnGithub = document.getElementById('btnGithub');
-const btnBehance = document.getElementById('btnBehance')
-
-btnLinkedin.addEventListener('click', () => {
-    window.open("https://www.linkedin.com/in/alice-ram%C3%ADrez-developer/", "_blank")
+btnDocs9.addEventListener("click", () => {
+  window.open(
+    "https://github.com/AliceRamirez17/LIM012-fe-burger-queen-api-client/tree/wip-alice",
+    "_blank"
+  );
 });
 
-btnGithub.addEventListener('click', () => {
-    window.open("https://github.com/AliceRamirez17", "_blank")
+const btnLinkedin = document.getElementById("btnLinkedin");
+const btnGithub = document.getElementById("btnGithub");
+const btnBehance = document.getElementById("btnBehance");
+
+btnLinkedin.addEventListener("click", () => {
+  window.open(
+    "https://www.linkedin.com/in/alice-ram%C3%ADrez-developer/",
+    "_blank"
+  );
 });
 
-btnBehance.addEventListener('click', () => {
-    window.open("https://www.behance.net/thecatgirl", "_blank")
+btnGithub.addEventListener("click", () => {
+  window.open("https://github.com/AliceRamirez17", "_blank");
 });
 
+btnBehance.addEventListener("click", () => {
+  window.open("https://www.behance.net/thecatgirl", "_blank");
+});
