49415b6f1e18fea88331501533b71e93bfeb265f
Alice <alicemarcelaramirez@gmail.com>
2025-02-18 20:35:45 -0500

    Portafolio actualizado

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ const btnDocs6 = document.getElementById("btn-6");
 const btnDocs7 = document.getElementById("btn-7");
 const btnDocs9 = document.getElementById("btn-9");
 const btnDocs11 = document.getElementById("btn-11");
+const btnDocs12 = document.getElementById("btn-12");
 const btnDocs13 = document.getElementById("btn-13");
 const btnDocs14 = document.getElementById("btn-14");
+const btnDocs15 = document.getElementById("btn-15");
+const btnDocs16 = document.getElementById("btn-16");
+const btnDocs17 = document.getElementById("btn-17");
 
 btnDocs3.addEventListener("click", () => {
   window.open("https://github.com/AliceRamirez17/LIM012-data-lovers", "_blank");
@@ -38,15 +42,31 @@ btnDocs7.addEventListener("click", () => {
 });
 
 btnDocs11.addEventListener("click", () => {
-  window.open("https://winagperu.org/", "_blank");
+  window.open("https://www.behance.net/gallery/201383967/Landing-Page-UX-UI-Organic-Beauty", "_blank");
+});
+
+btnDocs12.addEventListener("click", () => {
+  window.open("https://www.behance.net/gallery/201460835/Landing-Page-Tiendas-Mass-UX-UI", "_blank");
 });
 
 btnDocs13.addEventListener("click", () => {
-  window.open("https://devonvuejs.000webhostapp.com/mass/", "_blank");
+  window.open("https://www.tiendasmass.com.pe/", "_blank");
 });
 
 btnDocs14.addEventListener("click", () => {
-  window.open("https://www.tiendasmass.com.pe/", "_blank");
+  window.open("https://www.behance.net/gallery/201535843/Landing-Page-Lexington-Apartments-UX-UI", "_blank");
+});
+
+btnDocs15.addEventListener("click", () => {
+  window.open("https://lexingtongarden.com/", "_blank");
+});
+
+btnDocs16.addEventListener("click", () => {
+  window.open("https://kiviu.com/", "_blank");
+});
+
+btnDocs17.addEventListener("click", () => {
+  window.open("https://cuandojugamos.com/", "_blank");
 });
 
 const btnLinkedin = document.getElementById("btnLinkedin");
